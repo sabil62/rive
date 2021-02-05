3cb7c594ee51c1d9779480ecf30934fe71586e5c
sabil62 <shresthasabil62@gmail.com>
2021-02-05 14:02:40 +0545

    stream clss

---
+++ apple/lib/stream/streamHttpClass.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StreamHTTPwithClass extends StatefulWidget {
+  @override
+  _StreamHTTPwithClassState createState() => _StreamHTTPwithClassState();
+}
+
+class _StreamHTTPwithClassState extends State<StreamHTTPwithClass> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
