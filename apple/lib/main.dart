302ee5f73e0d0718e88e1aa97b24c91e7b8467c6
sabil62 <shresthasabil62@gmail.com>
2021-03-17 22:34:17 +0545

    debug banner removed

--- apple/lib/main.dart
+++ apple/lib/main.dart
@@ -39,7 +39,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Apple',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
